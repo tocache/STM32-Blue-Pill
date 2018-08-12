9ecafc2bde72cd00b6fd3bde8459f5120fff07a9
Kalun Lau <klnlau@gmail.com>
2018-08-12 01:26:53 -0500

    Add files via upload

---
+++ 09082018_stm32bluepill_onoffled/09082018_stm32bluepill_onoffled.ino
@@ -0,0 +1,42 @@
+#include <LiquidCrystal.h>
+
+const int rs = PA4, en = PA5, d4 = PA0, d5 = PA1, d6 = PA2, d7 = PA3;
+int contador = 0;
+int deccont = 0;
+int unicont = 0;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);           //Tamaño del LCD 16x2
+  pinMode(PA6, OUTPUT);
+  pinMode(PC13, OUTPUT);
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("Kalun Lau Gan "); //Imprime el mensaje 
+  digitizer();
+  lcd.print(deccont, DEC);
+  lcd.print(unicont, DEC);
+  lcd.setCursor(0,1);
+  lcd.print("STM32duino Test");
+  digitalWrite(PA6, HIGH);
+  digitalWrite(PC13, HIGH);
+  delay(100);
+  digitalWrite(PA6, LOW);
+  digitalWrite(PC13, LOW);
+  delay(100);
+  if(contador > 98){
+    contador = 0;
+  }
+  else{
+    contador = contador +1;
+  }
+}
+
+void digitizer()  {
+  deccont = contador / 10;
+  unicont = contador - (deccont * 10);
+}
+
---
+++ 10082018_stm32bluepill_ds18b20lcd/10082018_stm32bluepill_ds18b20lcd.ino
@@ -0,0 +1,33 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+#define ONE_WIRE_BUS PB5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+float Celcius=0;
+
+const int rs = PA4, en = PA5, d4 = PA0, d5 = PA1, d6 = PA2, d7 = PA3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  lcd.begin(16, 2);           //Tamaño del LCD 16x2
+  pinMode(PA6, OUTPUT);
+  pinMode(PC13, OUTPUT);
+}
+
+void loop(void)
+{ 
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  lcd.setCursor(0,0);
+  lcd.print("STM32duino 2018");
+  lcd.setCursor(0,1);
+  lcd.print("DS18B20: ");
+  lcd.print(Celcius);
+  lcd.write(0xDF);
+  lcd.print("C");
+  delay(500);
+}
