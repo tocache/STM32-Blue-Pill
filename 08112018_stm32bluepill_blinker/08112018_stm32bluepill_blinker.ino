d4053235a1ecc21cc11de2f32d051789a33c745c
Kalun Lau <klnlau@gmail.com>
2018-11-08 10:10:29 -0500

    blinker test on PC13 port (onboard LED)

---
+++ 08112018_stm32bluepill_blinker/08112018_stm32bluepill_blinker.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PC13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PC13, LOW);
+  delay(50);
+  digitalWrite(PC13, HIGH);
+  delay(80);
+  digitalWrite(PC13, LOW);
+  delay(50);
+  digitalWrite(PC13, HIGH);
+  delay(1200);
+}
